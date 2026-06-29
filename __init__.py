ee1ec42673925858b20442f56abeac1c577d1fec
dantweb <daniil.tkachev@gmail.com>
2026-06-29 09:08:47 +0200

    post release updates

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ class BotBasePlugin(BasePlugin):
     def metadata(self) -> PluginMetadata:
         return PluginMetadata(
             name="bot-base",
-            version="1.0.0",
+            version="26.6",
             author="VBWD Team",
             description=(
                 "Provider-neutral bot bridge: messenger-provider registry, "
